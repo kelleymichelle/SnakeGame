d5ea69215525736df93391a41a022e0b561bbeba
kelleymichelle <kmchaplain@gmail.com>
2020-10-16 22:48:34 -0500

    started helper methods for drawing the apple and also creating random coordinates for apple

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ window.onload = () => {
 
   let frames = 8
 
+  redrawApple()
   
   setInterval(() => {
-    document.addEventListener('keydown', gameControl)
+    document.addEventListener('keydown', (e) => gameControl(e))
     draw()
   } , 1000/frames)
 }
@@ -40,17 +41,34 @@ function draw() {
   context.fillRect(0, 0, canvas.width, canvas.height)
 
   for (let i = 0; i < snake.length; i++) {
-    console.log("should draw snake")
+    // console.log("should draw snake")
     context.fillStyle = 'magenta'
     context.fillRect(snake[i].x, snake[i].y, snakeLink, snakeLink)
   }
-
+  renderApple()
 }
 
-function gameControl() {
-  console.log("game control")
+function gameControl(e) {
+  console.log(e.keyCode)
+  // up = 38
+  //down = 40
+  //left = 37
+  //right = 39
 }
 
 function renderApple() {
-  
+
+  context.fillStyle = 'red'
+  context.beginPath()
+  context.arc(apple.x, apple.y, 10, 0, Math.PI * 2, true)
+  context.fill()
+
+}
+
+function redrawApple() {
+
+  let appleX = Math.floor(Math.random() * (1000 - 20) + 20);
+  let appleY = Math.floor(Math.random() * (800 - 20) + 20);
+
+  apple = {x: appleX, y: appleY}
 }
